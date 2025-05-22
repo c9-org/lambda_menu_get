7e6c015ff8b116349bf0911c7691a3fd46f00684
somnath <somnathbm0@gmail.com>
2025-05-22 07:27:17 +0530

    added moe test

--- tests/unit/test_menu_get.py
+++ tests/unit/test_menu_get.py
@@ -1,13 +1,24 @@
 import json
 import pytest
+from moto import mock_aws
 from aws_lambda_powertools.utilities.data_classes import APIGatewayProxyEvent
-
+from src.app import lambda_handler
 
 @pytest.fixture()
 def apigw_event() ->  APIGatewayProxyEvent:
-    with open(f"events/unit-test-event.json") as f:
+    with open(f"../events/unit-test-event.json") as f:
         return APIGatewayProxyEvent(json.load(f))
     
 
-def test_lambda_handler(apigw_event: dict) -> None:
-    pass
+@mock_aws
+def test_event_type_exists(apigw_event: dict) -> None:
+    response = lambda_handler(apigw_event, "")
+    print(response)
+    assert response.get('statusCode') == 200
+
+
+@mock_aws
+def test_menu_exists(apigw_event: dict) -> None:
+    response = lambda_handler(apigw_event, "")
+    items = response.get('body')['Items']
+    assert len(items) > 0
